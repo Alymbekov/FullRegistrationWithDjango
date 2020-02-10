b1af1e0d9f2330745a28e46b17da832919bd350b
Alymbekov <maxim.makarov.1997@mail.ru>
2020-02-10 20:18:18 +0600

    add signup view

--- apps/mysite/urls.py
+++ apps/mysite/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import IndexListView
+app_name = "mysite"
 
 urlpatterns = [
     #путь для загрузки главной страницы
--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('apps.mysite.urls')),
+    path('', include('apps.users.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
